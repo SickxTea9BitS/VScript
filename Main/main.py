ac31c42d1a4c7d07ac6a5d2cb372e190b9a4c89c
x0x_Devil_x0x <71588719+x0xDevilx0x@users.noreply.github.com>
2023-10-11 00:02:08 +0530

    Initial main.py code created

--- Main/main.py
+++ Main/main.py
@@ -1,2 +1,27 @@
 #Start
 
+import speech_recognition as sr
+import pyttsx3
+
+r = sr.Recognizer()
+
+def speakText(command):
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+while True:  # Changed while(1) to while True for an infinite loop
+    try:
+        with sr.Microphone() as source2:
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            audio2 = r.listen(source2)
+            mytext = r.recognize_google(audio2)
+            mytext = mytext.lower()
+
+            print("Did you say", mytext)
+            speakText(mytext)
+    except sr.RequestError as e:
+        print("Could not request result; {0}".format(e))  # Fixed the typo
+
+    except sr.UnknownValueError:
+        print("Unknown error occurred")
